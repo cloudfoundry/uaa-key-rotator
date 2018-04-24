b3201b64c8a3fe0f09d22e708e1d7b793fc051f1
Dennis Leon <dleon@pivotal.io>
2018-04-24 14:31:09 -0700

    Add logic to get encryptor/decryptor for a specific key label
    [#155302354] https://www.pivotaltracker.com/story/show/155302354
    
    Signed-off-by: Jennifer Hamon <jhamon@pivotal.io>

---
+++ rotator/key_service.go
@@ -0,0 +1,56 @@
+package rotator
+
+import (
+	"github.com/cloudfoundry/uaa-key-rotator/crypto"
+	"github.com/pkg/errors"
+	"fmt"
+)
+
+type EncryptionKey struct {
+	Label      string
+	Passphrase string
+}
+
+type UaaKeyService struct {
+	ActiveKeyLabel string
+	EncryptionKeys []EncryptionKey
+}
+
+func (s UaaKeyService) Key(keyLabel string) (crypto.Decryptor, error) {
+	var key EncryptionKey
+	var found bool
+
+	if found, key = s.getEncryptionKey(keyLabel); !found {
+		return crypto.UAADecryptor{}, errors.New(fmt.Sprintf("unable to find key: %s", keyLabel))
+
+	}
+
+	return crypto.UAADecryptor{
+		Passphrase: key.Passphrase,
+	}, nil
+}
+
+func (s UaaKeyService) ActiveKey() (string, crypto.Encryptor) {
+	var key EncryptionKey
+	var found bool
+
+	if found, key = s.getEncryptionKey(s.ActiveKeyLabel); !found {
+		return "", nil
+	}
+
+	return s.ActiveKeyLabel, crypto.UAAEncryptor{
+		Passphrase:     key.Passphrase,
+		SaltGenerator:  crypto.UaaSaltGenerator{},
+		NonceGenerator: crypto.UaaNonceGenerator{},
+	}
+}
+
+func (s UaaKeyService) getEncryptionKey(label string) (bool, EncryptionKey) {
+	for _, key := range s.EncryptionKeys {
+		if key.Label == label {
+			return true, key
+		}
+	}
+
+	return false, EncryptionKey{}
+}
---
+++ rotator/key_service_test.go
@@ -0,0 +1,66 @@
+package rotator_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/cloudfoundry/uaa-key-rotator/rotator"
+)
+
+var _ = Describe("KeyService", func() {
+	var uaaKeyService rotator.UaaKeyService
+
+	BeforeEach(func() {
+		uaaKeyService = rotator.UaaKeyService{
+			ActiveKeyLabel: "active-key-label",
+			EncryptionKeys: []rotator.EncryptionKey{
+				{Label: "active-key-label", Passphrase: "passphrase1"},
+				{Label: "key-2", Passphrase: "passphrase2"},
+			},
+		}
+	})
+
+	It("should return the correct active key", func() {
+		activeKeyLabel, _ := uaaKeyService.ActiveKey()
+
+		Expect(activeKeyLabel).To(Equal(activeKeyLabel))
+	})
+
+	It("should be the identity to encrypt and then decrypt", func() {
+		plainText := "some random plain text"
+		activeKeyLabel, activeKeyEncryptor := uaaKeyService.ActiveKey()
+		activeKeyDecryptor, err := uaaKeyService.Key(activeKeyLabel)
+		Expect(err).NotTo(HaveOccurred())
+
+		encryptedValue, err := activeKeyEncryptor.Encrypt(plainText)
+		Expect(err).NotTo(HaveOccurred())
+
+		decryptedValue, err := activeKeyDecryptor.Decrypt(encryptedValue)
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(decryptedValue).To(Equal(plainText))
+	})
+
+	Context("when encrypting / decrypting with different keys", func() {
+		It("should return a meaningful error", func() {
+			plainText := "some random plain text"
+			_, activeKeyEncryptor := uaaKeyService.ActiveKey()
+			activeKeyDecryptor, err := uaaKeyService.Key("key-2")
+			Expect(err).NotTo(HaveOccurred())
+
+			encryptedValue, err := activeKeyEncryptor.Encrypt(plainText)
+			Expect(err).NotTo(HaveOccurred())
+
+			decryptedValue, err := activeKeyDecryptor.Decrypt(encryptedValue)
+			Expect(err).To(HaveOccurred())
+			Expect(decryptedValue).NotTo(Equal(plainText))
+		})
+	})
+
+	Context("when asking for a key that does not exist", func() {
+		It("should return a meaningful error", func() {
+			_, err := uaaKeyService.Key("key-does-not-exist")
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(MatchError("unable to find key: key-does-not-exist"))
+		})
+	})
+})
