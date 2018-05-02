6be3d9c3f87af996c4cc8bf1cb840eafe283594d
Jaskanwal Pawar <jaskanwal.pawar@gmail.com>
2018-05-01 17:34:20 -0700

    Add ci task for mysql/postgresql
    [#155302354] https://www.pivotaltracker.com/story/show/155302354
    
    Signed-off-by: Dennis Leon <dleon@pivotal.io>

---
+++ ci/tasks/run-unit-tests-mysql/task.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+source uaa/scripts/start_db_helper.sh
+bootDB $DB_SCHEME
+
+set -eux
+
+export GOPATH=$PWD/go
+export PATH=$PATH:$GOPATH/bin
+
+mkdir -p go/src/github.com/cloudfoundry/uaa-key-rotator
+cp -R uaa-key-rotator/* go/src/github.com/cloudfoundry/uaa-key-rotator
+cd go/src/github.com/cloudfoundry/uaa-key-rotator
+
+go get github.com/onsi/ginkgo/ginkgo
+
+reformatted_packages=$(go fmt ...)
+if [[ $reformatted_packages = *[![:space:]]* ]]; then
+  echo "FAILURE: go fmt reformatted the following packages:"
+  echo $reformatted_packages
+  exit 1
+fi
+
+
+ginkgo -r --race -randomizeAllSpecs -randomizeSuites .
--- ci/tasks/run-unit-tests/task.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-source uaa/scripts/start_db_helper.sh
-bootDB postgresql
